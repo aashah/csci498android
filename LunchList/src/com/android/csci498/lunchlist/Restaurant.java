50124195194463d2932e793abd8c3e713d063647
Aakash Shah <ashah@mines.edu>
2012-08-31 18:16:54 -0600

    Added Restaurant

---
+++ LunchList/src/com/android/csci498/lunchlist/Restaurant.java
@@ -0,0 +1,20 @@
+package com.android.csci498.lunchlist;
+
+public class Restaurant {
+	private String name = "";
+	private String address = "";
+	
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
