3828842565f7a97fa3964cb3f496bea041d24afb
Aakash Shah <ashah@mines.edu>
2012-09-06 14:13:02 -0600

    Lesson 5

--- LunchList/src/com/android/csci498/lunchlist/LunchList.java
+++ LunchList/src/com/android/csci498/lunchlist/LunchList.java
@@ -4,21 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
-import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
+import android.widget.TextView;
 
 public class LunchList extends Activity {
 	
 	List<Restaurant> model = new ArrayList<Restaurant>();
-	ArrayAdapter<Restaurant> adapter = null;
+	RestaurantAdapter adapter = null;
 	
 	
     @Override
@@ -29,23 +31,9 @@ public class LunchList extends Activity {
         Button save = (Button) findViewById(R.id.save);
         save.setOnClickListener(onSave);
         
-        Spinner list = (Spinner) findViewById(R.id.restaurants);
-        adapter = new ArrayAdapter<Restaurant>(this,
-        										android.R.layout.simple_list_item_1,
-        										model);
+        ListView list = (ListView) findViewById(R.id.restaurants);
+        adapter = new RestaurantAdapter();
         list.setAdapter(adapter);
-        
-        EditText name = (EditText) findViewById(R.id.name);
-		EditText address = (EditText) findViewById(R.id.addr);
-		
-        Typeface consolasType = Typeface.createFromAsset(getAssets(), "fonts/CONSOLA.TTF");
-        name.setTypeface(consolasType);
-        address.setTypeface(consolasType);
-        
-        RadioGroup types = (RadioGroup) findViewById(R.id.types);
-        RadioButton rb = new RadioButton(this);
-        rb.setText("Test");
-        types.addView(rb);
         
     }
 
@@ -79,11 +67,52 @@ public class LunchList extends Activity {
         return true;
     }
     
+    static class RestaurantHolder {
+    	private TextView name = null;
+    	private TextView address = null;
+    	private ImageView icon = null;
+    	
+    	RestaurantHolder(View row) {
+    		name = (TextView) row.findViewById(R.id.title);
+    		address = (TextView) row.findViewById(R.id.address);
+    		icon = (ImageView) row.findViewById(R.id.icon);
+    	}
+    	
+    	void populateFrom(Restaurant r) {
+    		name.setText(r.getName());
+    		address.setText(r.getAddress());
+    		if (r.getType().equals("sit_down")) {
+    			icon.setImageResource(R.drawable.ball_red);
+    		} else if (r.getType().equals("take_out")) {
+    			icon.setImageResource(R.drawable.ball_yellow);
+    		} else {
+    			icon.setImageResource(R.drawable.ball_green);
+    		}
+    	}
+    }
+    
     class RestaurantAdapter extends ArrayAdapter<Restaurant> {
     	  RestaurantAdapter() {
     	    super(LunchList.this,
     	          android.R.layout.simple_list_item_1,
     	          model);
+    	  }
+    	  public View getView(int position, View convertView, ViewGroup parent) {
+    		  View row = convertView;
+    		  RestaurantHolder holder = null;
+    		  
+    		  if (row == null) {
+    			  LayoutInflater inflater = getLayoutInflater();
+    			  row = inflater.inflate(R.layout.row, null);
+    			  
+    			  holder = new RestaurantHolder(row);
+    			  row.setTag(holder);    			  
+    		  } else {	
+    			  holder = (RestaurantHolder)row.getTag();
+    		  }
+    		  
+    		  holder.populateFrom(model.get(position));
+    		  return row;
     	  }
     	}
 }
