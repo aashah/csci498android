5f05368eb97a5514e748eead358e80096ef7c6d0
Aakash Shah <ashah@mines.edu>
2012-10-06 16:14:31 -0600

    Moved feed processing to an IntentService (FeedService) as per #16.2

--- LunchList/src/com/android/csci498/lunchlist/FeedService.java
+++ LunchList/src/com/android/csci498/lunchlist/FeedService.java
@@ -1,16 +1,27 @@
 package com.android.csci498.lunchlist;
 
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedService extends IntentService {
 
+	public static final String EXTRA_URL = "apt.tutorial.EXTRA_URL";
 	public FeedService() {
 		super("FeedService");
 	}
 	
 	@Override
 	public void onHandleIntent(Intent i) {
+		RSSReader reader = new RSSReader();
 		
+		try {
+			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+		} catch (Exception e) {
+			Log.e("LunchList", "Error parsing feed", e);
+		}
 	}
 }
