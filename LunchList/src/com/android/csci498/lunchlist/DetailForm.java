768a5ca9d9cf4f9b6934ace8ca7c92c89049a65b
Aakash Shah <ashah@mines.edu>
2012-10-06 12:40:16 -0600

    Added the feed URL to the form as well as further functionality for using the feed String as per #15.2

--- LunchList/src/com/android/csci498/lunchlist/DetailForm.java
+++ LunchList/src/com/android/csci498/lunchlist/DetailForm.java
@@ -12,14 +12,15 @@ public class DetailForm extends Activity {
 	
 	
 	
-	EditText name = null;
-	EditText address = null;
-	EditText notes = null;
-	RadioGroup types = null;
+	EditText name;
+	EditText address;
+	EditText notes;
+	EditText feed;
+	RadioGroup types;
 	
-	RestaurantHelper helper = null;
+	RestaurantHelper helper;
 	
-	String restaurantId = null;
+	String restaurantId;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -39,15 +40,19 @@ public class DetailForm extends Activity {
     	state.putString("address", address.getText().toString());
     	state.putString("notes", notes.getText().toString());
     	state.putInt("type", types.getCheckedRadioButtonId());
+    	state.putString("feed", feed.getText().toString());
     	
     }
     
     @Override
     public void onRestoreInstanceState(Bundle state) {
+    	
     	name.setText(state.getString("name"));
     	address.setText(state.getString("address"));
     	notes.setText(state.getString("notes"));
     	types.check(state.getInt("type"));
+    	feed.setText(state.getString("feed"));
+    	
     }
     
 	public void retrieveFormHandlers() {
@@ -55,19 +60,25 @@ public class DetailForm extends Activity {
 		name = (EditText) findViewById(R.id.name);
 		address = (EditText) findViewById(R.id.addr);
 		notes = (EditText) findViewById(R.id.notes);
+		feed = (EditText) findViewById(R.id.feed); 
 		types = (RadioGroup) findViewById(R.id.types);
+		
 	}
 	public void registerEventHandlers() {
+		
 		helper = new RestaurantHelper(this);
 		Button save = (Button) findViewById(R.id.save);
 		save.setOnClickListener(onSave);
+		
 	}
 	
 	public void processIntentData() {
+		
 		restaurantId = getIntent().getStringExtra(LunchList.ID_EXTRA);
 		if (restaurantId != null) {
 			loadFromId();
 		}
+		
 	}
 	
 	public void loadFromId() {
@@ -77,6 +88,7 @@ public class DetailForm extends Activity {
 		name.setText(helper.getName(c));
 		address.setText(helper.getAddress(c));
 		notes.setText(helper.getNotes(c));
+		feed.setText(helper.getFeed(c));
 		
 		if (helper.getType(c).equals("@string/type_sit_down")) {
 			types.check(R.id.sit_down);
@@ -114,12 +126,14 @@ public class DetailForm extends Activity {
 						name.getText().toString(), 
 						address.getText().toString(), 
 						type, 
-						notes.getText().toString());
+						notes.getText().toString(),
+						feed.getText().toString());
 			} else {
 				helper.insert(name.getText().toString(), 
 						address.getText().toString(), 
 						type, 
-						notes.getText().toString());
+						notes.getText().toString(),
+						feed.getText().toString());
 			}
 			finish();
 		}
