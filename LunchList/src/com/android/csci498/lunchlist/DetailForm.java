65e986e1bf56410686aabc0b73dc8c6f00fbdf20
Aakash Shah <ashah@mines.edu>
2012-10-06 13:13:12 -0600

    Added menu event handlers which check network connectivity & added request for permissions to do so in manifest file as per #15.4

--- LunchList/src/com/android/csci498/lunchlist/DetailForm.java
+++ LunchList/src/com/android/csci498/lunchlist/DetailForm.java
@@ -1,14 +1,19 @@
 package com.android.csci498.lunchlist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class DetailForm extends Activity {
 	
@@ -63,6 +68,26 @@ public class DetailForm extends Activity {
     	return (super.onCreateOptionsMenu(menu));
     }
     
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	if (item.getItemId() == R.id.feed) {
+    		if (isNetworkAvailable()) {
+    			Intent i = new Intent(this, FeedActivity.class);
+    			i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
+    			startActivity(i);
+    		} else {
+    			Toast.makeText(this, "Sorry, the Internet is not available", Toast.LENGTH_LONG).show();
+    		}
+    		return true;
+    	}
+    	return super.onOptionsItemSelected(item);
+    }
+    
+    public boolean isNetworkAvailable() {
+    	ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+    	NetworkInfo info = cm.getActiveNetworkInfo();
+    	return (info != null);
+    }
 	public void retrieveFormHandlers() {
 		
 		name = (EditText) findViewById(R.id.name);
