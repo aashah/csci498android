eae0827e1f76be66040c5d52d8fafdc2b83bab63
Aakash Shah <ashah@mines.edu>
2012-10-30 18:53:08 -0600

    Added code to actually request GPA location and update UI TextView to display it

--- LunchList/src/com/android/csci498/lunchlist/DetailForm.java
+++ LunchList/src/com/android/csci498/lunchlist/DetailForm.java
@@ -3,6 +3,9 @@ package com.android.csci498.lunchlist;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -24,6 +27,7 @@ public class DetailForm extends Activity {
 	TextView location;
 	
 	RestaurantHelper helper;
+	LocationManager locMgr;
 	
 	String restaurantId;
 	
@@ -40,6 +44,7 @@ public class DetailForm extends Activity {
 	@Override
 	public void onPause() {
 		save();
+		locMgr.removeUpdates(onLocationChange);
 		super.onPause();
 	}
 	
@@ -83,6 +88,8 @@ public class DetailForm extends Activity {
     			Toast.makeText(this, "Sorry, the Internet is not available", Toast.LENGTH_LONG).show();
     		}
     		return true;
+    	} else if (item.getItemId() == R.id.location) {
+    		locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, onLocationChange);
     	}
     	return super.onOptionsItemSelected(item);
     }
@@ -103,9 +110,8 @@ public class DetailForm extends Activity {
 		
 	}
 	public void registerEventHandlers() {
-		
 		helper = new RestaurantHelper(this);
-		
+		locMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
 	}
 	
 	public void processIntentData() {
@@ -173,4 +179,28 @@ public class DetailForm extends Activity {
 					feed.getText().toString());
 		}
 	}
+
+	LocationListener onLocationChange = new LocationListener() {
+
+		@Override
+		public void onLocationChanged(Location fix) {
+			helper.updateLocation(restaurantId, fix.getLatitude(), fix.getLongitude());
+			location.setText(String.valueOf(fix.getLatitude())
+					+ ", "
+                    + String.valueOf(fix.getLongitude()));
+			locMgr.removeUpdates(onLocationChange);
+			
+			Toast.makeText(DetailForm.this, "Location saved", Toast.LENGTH_LONG).show();	
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) { }
+
+		@Override
+		public void onProviderEnabled(String provider) { }
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) { }
+		
+	};
 }
