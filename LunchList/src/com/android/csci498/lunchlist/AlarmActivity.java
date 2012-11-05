79bdc18b79431f82fa05b2d74b631f2b5cc6f82f
Aakash Shah <ashah@mines.edu>
2012-11-04 20:40:54 -0700

    Added our AlarmActivity to the manifest file. Created our ALarmReceiver to handle what happens when the alarm executes alongside it's respective layout file per #19.4. Completed APT Tutorial 19

---
+++ LunchList/src/com/android/csci498/lunchlist/AlarmActivity.java
@@ -0,0 +1,13 @@
+package com.android.csci498.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlarmActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.alarm);
+	}
+}
