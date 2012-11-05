eba707045f5a2009e42ac0d1985c46648fc8e34d
Aakash Shah <ashah@mines.edu>
2012-11-04 20:01:40 -0700

    Added a BootReceiver to handle boots & included this into our manifest file per #19.3

---
+++ LunchList/src/com/android/csci498/lunchlist/OnBootReceiver.java
@@ -0,0 +1,15 @@
+package com.android.csci498.lunchlist;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		
+
+	}
+
+}
