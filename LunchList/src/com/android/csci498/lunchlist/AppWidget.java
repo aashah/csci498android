16c2388deaaacd3b1a004ebc57f063bba2218275
Aakash Shah <ashah@mines.edu>
2012-11-23 16:39:58 -0700

    Completed ListWidgetService (#22.4) and added the service to the manifest file

--- LunchList/src/com/android/csci498/lunchlist/AppWidget.java
+++ LunchList/src/com/android/csci498/lunchlist/AppWidget.java
@@ -1,9 +1,12 @@
 package com.android.csci498.lunchlist;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
+import android.widget.RemoteViews;
 
 
 public class AppWidget extends AppWidgetProvider {
@@ -13,5 +16,28 @@ public class AppWidget extends AppWidgetProvider {
 		} else {
 			ctxt.startService(new Intent(ctxt, WidgetService.class));
 		}
+	}
+
+	private void onHCUpdate(Context ctxt, AppWidgetManager mgr,
+			int[] appWidgetIds) {
+		for (int i = 0; i < appWidgetIds.length; ++i) {
+			Intent svcIntent = new Intent(this, ListWidgetService.class);
+			
+			svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[1]);
+			svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
+			
+			RemoteViews widget = new RemoteViews(ctxt.getPackageName(), R.layout.widget);
+			
+			widget.setRemoteAdapter(appWidgetIds[i], R.id.restaurants, svcIntent);
+			
+			Intent clickIntent = new Intent(ctxt, DetailForm.class);
+			PendingIntent pi = PendingIntent.getActivity(ctxt, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+			
+			widget.setPendingIntentTemplate(R.id.restaurants, pi);
+			
+			mgr.updateAppWidget(appWidgetIds[i], widget);
+		}
+		
+		super.onUpdate(ctxt, mgr, appWidgetIds);
 	}
 }
---
+++ LunchList/src/com/android/csci498/lunchlist/ListWidgetService.java
@@ -0,0 +1,13 @@
+package com.android.csci498.lunchlist;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class ListWidgetService extends RemoteViewsService {
+
+	@Override
+	public RemoteViewsFactory onGetViewFactory(Intent intent) {
+		return new ListViewsFactory(this.getApplicationContext(), intent);
+	}
+
+}
