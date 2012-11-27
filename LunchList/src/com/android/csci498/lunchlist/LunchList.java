d5bc28deab38923484d8a3a78b1504a0647bd384
Aakash Shah <ashah@mines.edu>
2012-11-26 23:01:53 -0700

    Added the routine to add a recycle a DetailForm if we are loading DetailForm into the side panel

--- LunchList/src/com/android/csci498/lunchlist/LunchList.java
+++ LunchList/src/com/android/csci498/lunchlist/LunchList.java
@@ -3,6 +3,8 @@ package com.android.csci498.lunchlist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 public class LunchList extends FragmentActivity
 						implements LunchFragment.OnRestaurantListener {
@@ -26,7 +28,20 @@ public class LunchList extends FragmentActivity
     		i.putExtra(ID_EXTRA, String.valueOf(id));
     		startActivity(i);
     	} else {
+    		FragmentManager fragMgr = getSupportFragmentManager();
+    		DetailFragment details = (DetailFragment) fragMgr.findFragmentById(R.id.details);
     		
+    		if (details == null) {
+    			details = DetailFragment.newInstance(id);
+    			FragmentTransaction xaction = fragMgr.beginTransaction();
+    			xaction
+    				.add(R.id.details, details)
+    				.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+    				.addToBackStack(null)
+    				.commit();    				
+    		} else {
+    			details.loadRestaurant(String.valueOf(id));
+    		}
     	}
 	}
 }
